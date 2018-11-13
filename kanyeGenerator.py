ce21a098d6909aa7857affbcf71398a7c29beab0
Reiss Jones <reiss1999@gmail.com>
2018-11-13 14:50:11 +0000

    Delete kanyeGenerator.py

--- kanyeGenerator.py
+++
@@ -1,8 +0,0 @@
-def kanyeGen(length):
-	words = ["woop", "de", "scoop", "poop", "diddy", "foop"]
-	lyric = ""
-	if length != 0:
-		lyric = kanyeGen(length-1)
-	num = random.randint(0, 5)
-	lyric += " "+words[num]
-	return lyric
