446c0b97b3b348d00b6529eebb101cda66e2fcb6
Reiss Jones <reiss1999@gmail.com>
2018-11-13 20:01:01 +0000

    Update kanyeGen.py

--- kanyeGen.py
+++ kanyeGen.py
@@ -4,7 +4,7 @@ def lyricGen(length):
 	words = ["woop", "de", "scoop", "poop", "diddy", "foop"]
 	lyric = ""
 	if length != 0:
-		lyric = lyrciGen(length-1)
+		lyric = lyricGen(length-1)
 	num = random.randint(0, 5)
 	lyric += " "+words[num]
 	return lyric
@@ -13,12 +13,12 @@ def paragraphGen(length):
 	paragraph = ""
 	if length != 0:
 		paragraph = paragraphGen(length-1)
-	paragraph += ""+lyricGen(1)
+	paragraph += ""+lyricGen(1)+"\n"
 	return paragraph
 
 def songGen(length = random.randint(3, 13), paragraphLength = random.randint(1, 15)):
 	song = ""
 	if length != 0:
-		song = song(length-1)
-	song += paragraphGen(paragraphLength)
+		song = songGen(length-1)
+	song += paragraphGen(paragraphLength)+"\n\n"
 	return song
